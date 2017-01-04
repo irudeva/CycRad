d8fd0037fafd31bb8014f63123520ac83cd801a2
irudeva <irinarudeva@gmail.com>
2017-01-05 00:48:17 +1100

    cos clean

--- scripts/getrad.py
+++ scripts/getrad.py
@@ -26,9 +26,6 @@ def polerot(plat,plon,ilat,ilon):
     plat = (plat-90.)*dtr
     plon = plon*dtr
 
-    print "ilat", ilat
-
-    print 'ilon=',ilon
     ilon[:] = [x*dtr for x in ilon]
     ilat[:] = [x*dtr for x in ilat]
 
@@ -37,10 +34,8 @@ def polerot(plat,plon,ilat,ilon):
 
     for index, (lon,lat) in enumerate(zip(ilon, ilat)):
 
-        # print lon*rtd,lat*rtd
-        print 'ind=',index
-        print "lat, lon = ",lat*rtd,lon*rtd
-        print "plat, plon = ",plat*rtd,plon*rtd
+        # print "lat, lon = ",lat*rtd,lon*rtd
+        # print "plat, plon = ",plat*rtd,plon*rtd
 
         # convert to cartesian coordinates
         # from bronstein p.217
@@ -54,61 +49,27 @@ def polerot(plat,plon,ilat,ilon):
 
         x=rad*np.cos(lat)*cos_lon
         y=rad*np.cos(lat)*np.sin(lon)
-        print "!!!!!!!!!np.cos(lon)", lon*rtd, 90, x
         z=rad*np.sin(lat)
-        # print "z, rad, sin(lat) =",z, rad, np.sin(lat)
-        print "x, y, z =", x,y,z
 
 
         # turn XY
         xn=x*cos_plon+y*np.sin(plon)
         yn=x*(-np.sin(plon))+y*cos_plon
-        print "x*(-np.sin(plon))+y*np.cos(plon)",x,(-np.sin(plon)),y,cos_plon
-        print "____yn", yn
         zn=z
-        # print "xn, yn, zn =", xn,yn,zn
 
         # tilt XZ
         xnn=xn*np.cos(plat)+zn*np.sin(plat)
-        # print "xn*np.cos(plat),zn*np.sin(plat)",xn*np.cos(plat),zn*np.sin(plat)
         ynn=yn
-        # print ynn/xnn
-        # print xnn/ynn
         znn=xn*(-np.sin(plat))+zn*np.cos(plat)
-        # print "xnn, ynn, znn =", xnn,ynn,znn
-        # print "plat, lat=",plat, lat
-        # print ilat
-        # print "zn,np.cos(plat)=",zn,np.cos(plat)
 
         # convert back to polar coordinates
         rr=np.sqrt(xnn*xnn+ynn*ynn+znn*znn)
         nlat[index]=np.arcsin(znn/rr)*rtd
 
 
-        # if xnn==0.:
-        #     print 'xnn = 0'
-        #     if ynn > 0.:
-        #       nlon[index]=0.
-        #     else:
-        # 	  nlon[index]=180.
-
-        # elif ynn==0.:
-        #     print 'ynn == 0'
-        #     print plon, x,y,yn
-        #     print x*(-np.sin(plon)),y*np.cos(plon)
-        #     nlon[index]=90.
-        # else:
         nlon[index]=np.arctan2(xnn,ynn)*rtd
-        print "xnn ynn =", xnn,ynn
-        print "xnn/ynn =", xnn/ynn
 
-        print "res----", nlat[index],nlon[index]
-
-    # if plat0>0 :
-    #      nlon = -nlon
-    # else :
-    #      nlat = -nlat
-
+        # print "res----", nlat[index],nlon[index]
 
     return nlat,nlon
 
@@ -222,25 +183,25 @@ for yr in range(2005,2006):
 
         #  print 'lon = ', lon[itrk,ip]
         #  print 'lat = ', lat[itrk,ip]
-         lon[itrk,ip] = 165
-         lat[itrk,ip] = -11
-        #  tmplat = [  90, -90, 15]
-        #  tmplon = [ 165, 165, 175]
-         tmplat = [  90, -90, -89.9, -89.9999]
-         tmplon = [ 165,165, 165, -15]
+         plon = lon[itrk,ip]
+         plat = lat[itrk,ip]
+         plon = 165
+         plat = -11
+         nplon = plon*1.
+         nplat = 90
+         tmplat = [ nplat, -90, -89.9, -89.9999]
+         tmplon = [ nplon,165, 165, -15]
          tmplat1 = tmplat*1
          tmplon1 = tmplon*1
-         nlat, nlon = polerot(lat[itrk,ip],lon[itrk,ip],tmplat,tmplon)
+         nlat, nlon = polerot(plat,plon,tmplat,tmplon)
          print 'lat = ', tmplat1
          print 'lon = ', tmplon1
          print 'nlat = ', nlat
          print 'nlon = ', nlon
-        #  print nlat[0],nlon[0],nlat[1],nlon[1]
-         print "place pole to (lat %f,lon %d)"%(nlat[0],nlon[0])
-         nlat1, nlon1 = polerot(nlat[0], nlon[0],nlat,nlon)
+         xnplat = nlat[0]
+         xnplon = nlon[0]
+         print "place pole to (lat %2.f,lon %d)"%(xnplat, xnplon)
+         nlat1, nlon1 = polerot(xnplat, xnplon,nlat,nlon)
          print 'nlat = ', nlat1
-        #  print 'nlon = ', nlon1
-        #  print 'nlon = ', nlon1+lon[itrk,ip] +90 # works for NH
-         print 'nlon = ', nlon1
-         print 'nlon = ', nlon1+90+lon[itrk,ip]
+         print 'nlon = ', nlon1+90+plon
 # quit()
